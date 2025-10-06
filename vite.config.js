2caf5a15c4c2d38da0a565c50cf0ef17370295f7
Jamy023 <164452079+Jamy023@users.noreply.github.com>
2025-10-06 15:47:00 -0500

    Add Tailwind CSS configuration and Vite setup for Laravel with Vue support
    - Created tailwind.config.js to configure Tailwind CSS with form plugin and specify content paths.
    - Added vite.config.temp.js for Vite configuration, integrating Laravel and Vue plugins for asset management and hot reloading.

--- vite.config.js
+++ vite.config.js
@@ -120,7 +120,7 @@ export default defineConfig({
                 ]
             },
             devOptions: {
-                enabled: true
+                enabled: false
             }
         })
     ],
